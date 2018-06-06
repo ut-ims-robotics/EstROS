3dd41a7323fd082c40e02a53615283a78b248bf3
Jan Bogdanov <jan.bogdanov@gmail.com>
2018-06-06 12:27:42 +0300

    Changed the size of the message queue to 3

--- est_voice_synth/src/estonian_text2speech.cpp
+++ est_voice_synth/src/estonian_text2speech.cpp
@@ -100,7 +100,7 @@ int main(int argc, char **argv)
    * is the number of messages that will be buffered up before beginning to throw
    * away the oldest ones.
    */
-  ros::Subscriber sub = n.subscribe("estonian_voice", 1000, chatterCallback);
+  ros::Subscriber sub = n.subscribe("estonian_voice", 3, chatterCallback);
 
   /**
    * ros::spin() will enter a loop, pumping callbacks.  With this version, all
